c5585b515e19bd1c6430497947867fbab111904d
Deviego <deviego4@gmail.com>
2023-01-21 21:19:56 -0300

    first step complete

--- src/bot.js
+++ src/bot.js
@@ -9,13 +9,10 @@ const rateLimit = require("axios-rate-limit");
 // Create a rate limiter with a limit of 10 requests per second
 const limiter = rateLimit(axios.create(), 10, 60000);
 
-
 const configuration = new Configuration({
   organization: "org-JLMIHF3WO2FV5SCBBcfjKdRe",
   apiKey: process.env.OPENAI_API_KEY,
 });
-
-
 
 const openai = new OpenAIApi(configuration);
 const client = new Client({
@@ -35,35 +32,34 @@ client.on("ready", () => {
 });
 
 client.on("message", async (message) => {
-  if (message.body ===  message.body) {
+  if (message.body === "oi") {
     client.sendMessage(
       message.from,
       "Olá sou o  Alfredozord,  secretário do Diego, ele não está disponível agora pode deixar uma mensagem obg"
     );
   } else {
-    
-// Use the rate-limited axios instance to make API calls
-limiter({
-  method: "post",
-  url: "https://api.openai.com/v1/completions",
-  params: {
-    model: "text-davinci-003",
-    prompt: message.body,
-    temperature: 0,
-    max_tokens: 7,
-  },
-  headers: {
-    Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    try {
+      // Use the rate-limited axios instance to make API calls
+      const response = await limiter({
+        method: "post",
+        url: "https://api.openai.com/v1/completions",
+        data: {
+          model: "text-davinci-003",
+          prompt: message.body,
+          temperature: 0,
+          max_tokens: 7,
+        },
+        headers: {
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+      });
+
+      // Handle the response from the OpenAI API
+      client.sendMessage(message.from, response.data.choices[0].text);
+    } catch (error) {
+      console.log(error);
+    }
   }
-}).then((response) => {
-    // Handle the response from the OpenAI API
-    console.log(response.data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-  }
-  
 });
 
 client.initialize();
